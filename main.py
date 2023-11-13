e75274e450a7b3bf9309f00207bd82ef0ef0868f
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-13 21:01:44 +0900

    commit

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import queue
 import generate, take
 
 job_queue = queue.Queue()
-
 generater = generate.Generater(job_queue)
 taker = take.Taker(job_queue)
 
+def gen_run():
+    generater.run()
+
+def tak_run():
+    taker.run()
+
 with poolExe() as pool:
-    pool.map([generater.run, taker.run])
+    pool.map([gen_run, tak_run])
